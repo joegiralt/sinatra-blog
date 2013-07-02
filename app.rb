812f6056203331da2d5d995a898e7e16a742a1ae
github username <github email address>
2013-07-02 18:15:44 -0400

    have base website grumpcat win

--- app.rb
+++ app.rb
@@ -14,9 +14,27 @@ module SinatraBlog
        
 
         get "/" do
-            @post = Post.order("create_at DESC") 
-            erb :"posts/index"
+          @posts = Post.order("created_at DESC") 
+          erb :"posts/index"
         end
+
+        helpers do
+          # If @title is assigned, add it to the page's title.
+          def title
+            if @title
+              "#{@title} -- My Blog"
+            else
+              "My Blog"
+            end
+          end
+
+          # Format the Ruby Time object returned from a post's created_at method
+          # into a string that looks like this: 06 Jan 2012
+          def pretty_date(time)
+           time.strftime("%d %b %Y")
+          end
+
+        end 
      
     end
 end
